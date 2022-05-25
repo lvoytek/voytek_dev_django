e0907d96e8d52615f86051e897dc09b6256418b9
Lena Voytek <lena@voytek.dev>
2022-05-25 16:39:30 -0700

    Add check for mobile

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
+from user_agents import parse
 
-context = {
-    'mobile': False
-}
+
+def get_context(request):
+    mobile = False
+    if hasattr(request, 'META'):
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+
+        if not isinstance(user_agent, str):
+            user_agent = user_agent.decode('utf-8', 'ignore')
+
+        mobile = parse(user_agent).is_mobile
+
+    return {"mobile": mobile}
 
 
 def index(request):
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', context=get_context(request))
 
 
 def resume(request):
-    return render(request, 'resume.html', context=context)
+    return render(request, 'resume.html', context=get_context(request))
 
 
 def about(request):
-    return render(request, 'about.html', context=context)
+    return render(request, 'about.html', context=get_context(request))
 
 
 def projects(request):
-    return render(request, 'projects.html', context=context)
+    return render(request, 'projects.html', context=get_context(request))
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='voytek-dev-django',
+    version='0.1.0',
+    packages=['pages', 'pages.migrations', 'voytek_dev_django'],
+    url='https://github.com/lvoytek/voytek_dev_django',
+    license='GPLv3',
+    author='Lena Voytek',
+    author_email='lena@voytek.dev',
+    description='voytek.dev created with Django',
+    install_requires=[
+        'user_agents'
+    ],
+)
