bd8d911b2ae14b26e0e8e20b2ec18a280d4fe62f
Lena Voytek <lena@voytek.dev>
2022-06-18 22:05:56 -0700

    Add project model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    link = models.URLField()
+    overview = models.CharField(max_length=4000)
+    image = models.ImageField()
+    tags = models.JSONField()
