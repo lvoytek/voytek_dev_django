fd47ae280f1232ebe3528129c3f3150e6917a234
Lena Voytek <lena@voytek.dev>
2022-07-06 11:08:59 -0700

    Fix landing page images and favicon

--- voytek_dev_django/urls.py
+++ voytek_dev_django/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static('/favicon.ico', path=settings.STATIC_ROOT + 'images/logo.png')
