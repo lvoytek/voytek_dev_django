18cc7c1f05496a03eb4d72a918e2e49c51030135
Lena Voytek <lena@voytek.dev>
2022-10-08 14:18:45 -0700

    Change sass location and update settings

--- voytek_dev_django/production_settings.py
+++ voytek_dev_django/production_settings.py
@@ -122,25 +122,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
-)
-
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
-COMPRESS_ENABLED = True
+STATICFILES_DIRS = [
+    BASE_DIR / "style"
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+)
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static/"
+
 COMPRESS_OFFLINE = True
 LIBSASS_OUTPUT_STYLE = 'compressed'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
