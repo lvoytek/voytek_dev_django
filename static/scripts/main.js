c8e73f167618505621100db845cd1f97e93e42f0
Lena Voytek <lena@voytek.dev>
2025-06-28 19:40:59 -0400

    Add comments to main script too

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,4 @@
+// Open or close the nav dropdown on smaller screens
 function toggleDropdown(toggle, open) {
   var parentElement = toggle.parentNode;
   var dropdown = document.getElementById(toggle.getAttribute('aria-controls'));
@@ -10,12 +11,14 @@ function toggleDropdown(toggle, open) {
   }
 }
 
+// Close all dropdown menus
 function closeAllDropdowns(toggles) {
   toggles.forEach(function(toggle) {
     toggleDropdown(toggle, false);
   });
 }
 
+// Close dropdown menu when clicking outside of it
 function handleClickOutside(toggles, containerClass) {
   document.addEventListener('click', function(event) {
     var target = event.target;
@@ -28,6 +31,7 @@ function handleClickOutside(toggles, containerClass) {
   });
 }
 
+// Nav menu dropdown initialization
 function initNavDropdowns(containerClass) {
   var toggles = [].slice.call(document.querySelectorAll(containerClass + ' [aria-controls]'));
 
